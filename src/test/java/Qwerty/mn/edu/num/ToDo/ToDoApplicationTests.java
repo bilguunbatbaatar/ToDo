a4d10046c8baf4336e052c76c81b82f76ea55771
Bilguun Batbaatar <bentoshka11@gmail.com>
2025-12-10 21:47:22 +0800

    first commit

---
+++ src/test/java/Qwerty/mn/edu/num/ToDo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package Qwerty.mn.edu.num.ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
